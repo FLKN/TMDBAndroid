5a198f01eb1f729c93b7f8796c2baa41564ec5c3
Gaston Michelan <gaston.emiliano.michelan@gmail.com>
2022-11-03 17:10:43 -0300

    gaston michelasn api solution

--- app/src/main/java/com/flknlabs/app1/MainActivity.kt
+++ app/src/main/java/com/flknlabs/app1/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
 import com.bumptech.glide.Glide
 import com.google.gson.Gson
 import retrofit2.Call
@@ -13,12 +16,21 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var imageView: ImageView
+    lateinit var movieTitle: TextView
+    lateinit var description: TextView
+    lateinit var info: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView = findViewById<ImageView>(R.id.imgPoster)
+        movieTitle = findViewById(R.id.movie_title)
+        imageView = findViewById(R.id.movie_poster)
+        description = findViewById(R.id.movie_description)
+        info = findViewById(R.id.movie_info)
+
+
+
     }
 
     override fun onResume() {
@@ -34,15 +46,40 @@ class MainActivity : AppCompatActivity() {
             .into(imageView)
     }
 
+    fun loadMovie(baseMovie: BaseResponse?){
+        baseMovie?.let{
+            loadImage(it.poster_path)
+            movieTitle.text = it.title
+            description.text = it.overview
+            createMovieInfo(it)
+        }?: run {
+
+        }
+    }
+
+    fun createMovieInfo(movie: BaseResponse){
+        info.text = buildSpannedString {
+            bold{ append("Language: ")}
+            appendLine(movie.original_language)
+
+            bold{ append("Original title: ")}
+            appendLine(movie.original_title)
+
+            bold { append("Release date: ") }
+            appendLine(movie.release_date)
+        }
+    }
+
     fun request() {
         val apiClient = ApiClient()
-        val call = apiClient.movieDatabaseAPI.getMovies(600, BuildConfig.API_KEY)
+        val call = apiClient.movieDatabaseAPI.getMovies(2, "d70583e78c3473c035fe05dee3c7c8b8")
 
         call.enqueue(object : Callback<BaseResponse> {
             override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
                 val json = Gson().toJson(response.body())
                 Log.d("MainActivity", "Response: $json")
-                loadImage(response.body()?.poster_path ?: "")
+                //loadImage(response.body()?.poster_path ?: "")
+                loadMovie(response.body())
             }
 
             override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
