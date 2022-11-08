55855522cc673126fc44f66bbeed61045633a358
David Perez <dpereze1105@hotmail.com>
2022-11-08 07:31:15 -0600

    Adding permissions to camera

--- app/src/main/java/com/flknlabs/app1/MainActivity.kt
+++ app/src/main/java/com/flknlabs/app1/MainActivity.kt
@@ -1,55 +1,63 @@
 package com.flknlabs.app1
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ImageView
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.google.gson.Gson
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
+const val CAMERA_REQUEST_CODE = 1234
 class MainActivity : AppCompatActivity() {
-    lateinit var imageView: ImageView
+    lateinit var btnCamera: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView = findViewById<ImageView>(R.id.imgPoster)
+        btnCamera = findViewById<Button>(R.id.btnCamera)
+        btnCamera.setOnClickListener { checkCameraPermission() }
     }
 
-    override fun onResume() {
-        super.onResume()
-        request()
-    }
-
-    fun loadImage(imagePath: String) {
-        Glide
-            .with(this)
-            .load(IMAGE_BASE_URL + imagePath)
-            .placeholder(R.mipmap.ic_launcher_round)
-            .into(imageView)
-    }
-
-    fun request() {
-        val apiClient = ApiClient()
-        val call = apiClient.movieDatabaseAPI.getMovies(600, BuildConfig.API_KEY)
-
-        call.enqueue(object : Callback<BaseResponse> {
-            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
-                val json = Gson().toJson(response.body())
-                Log.d("MainActivity", "Response: $json")
-                loadImage(response.body()?.poster_path ?: "")
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                //El usuario ya ha rechazado el permiso anteriormente, debemos informarle que vaya a ajustes.
+                Toast.makeText(this, "El usuario ya ha rechazado el permiso anteriormente", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+            } else {
+                //El usuario nunca ha aceptado ni rechazado, así que le pedimos que acepte el permiso.
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+                Toast.makeText(this, "El usuario nunca ha aceptado ni rechazado", Toast.LENGTH_SHORT).show()
             }
+        } else {
+            //El permiso está aceptado.
+            Toast.makeText(this, "El usuario ya habia aceptado el permiso", Toast.LENGTH_SHORT).show()
+        }
+    }
 
-            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
-                Log.d("MainActivity","Error: ${t.stackTrace}}")
-                loadImage( "")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //El usuario ha aceptado el permiso, no tiene porqué darle de nuevo al botón, podemos lanzar la funcionalidad desde aquí.
+                    Toast.makeText(this, "El usuario acepto el permiso", Toast.LENGTH_SHORT).show()
+                } else {
+                    //El usuario ha rechazado el permiso, podemos desactivar la funcionalidad o mostrar una vista/diálogo.
+                    Toast.makeText(this, "El usuario NOOOO acepto el permiso", Toast.LENGTH_SHORT).show()
+                }
+                return
             }
-        })
+            else -> {
+                // Este else lo dejamos por si sale un permiso que no teníamos controlado.
+                Toast.makeText(this, "Ocurrió un error inesperado", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
-
