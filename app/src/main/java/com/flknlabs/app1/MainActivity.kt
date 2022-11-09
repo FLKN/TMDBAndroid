91c1c71e473447412d82f87b5f50c4458aceebc3
David Perez <dpereze1105@hotmail.com>
2022-11-09 07:14:53 -0600

    Fixing errors from git

--- app/src/main/java/com/flknlabs/app1/MainActivity.kt
+++ app/src/main/java/com/flknlabs/app1/MainActivity.kt
@@ -1,20 +1,6 @@
 package com.flknlabs.app1
 
 import android.os.Bundle
-<<<<<<< Updated upstream
-import android.util.Log
-import android.view.View
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.google.gson.Gson
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
-class MainActivity : AppCompatActivity() {
-    lateinit var imageView: ImageView
-=======
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -34,47 +20,12 @@ class MainActivity : AppCompatActivity() {
     private var playWhenReady = true
     private var currentWindow = 0
     private var playbackPosition = 0L
->>>>>>> Stashed changes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-<<<<<<< Updated upstream
-        imageView = findViewById<ImageView>(R.id.imgPoster)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        request()
-    }
-
-    fun loadImage(imagePath: String) {
-        Glide
-            .with(this)
-            .load(IMAGE_BASE_URL + imagePath)
-            .placeholder(R.mipmap.ic_launcher_round)
-            .into(imageView)
-    }
-
-    fun request() {
-        val apiClient = ApiClient()
-        val call = apiClient.movieDatabaseAPI.getMovies(600, BuildConfig.API_KEY)
-
-        call.enqueue(object : Callback<BaseResponse> {
-            override fun onResponse(call: Call<BaseResponse>, response: Response<BaseResponse>) {
-                val json = Gson().toJson(response.body())
-                Log.d("MainActivity", "Response: $json")
-                loadImage(response.body()?.poster_path ?: "")
-            }
-
-            override fun onFailure(call: Call<BaseResponse>, t: Throwable) {
-                Log.d("MainActivity","Error: ${t.stackTrace}}")
-                loadImage( "")
-            }
-        })
-=======
     override fun onStart() {
         super.onStart()
         if (Util.SDK_INT >= 24) {
@@ -119,8 +70,6 @@ class MainActivity : AppCompatActivity() {
             release()
         }
         exoPlayer = null
->>>>>>> Stashed changes
     }
-
 }
 
